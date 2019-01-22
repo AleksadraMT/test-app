a8c17bbc8aebe35585c20450c3988440f81dcebf
aleksandramt <lawrhill.ml@gmail.com>
2019-01-22 10:06:20 +0200

    Updates

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import * as types from '../constants/ActionTypes'
-
-export const addTopic = text => ({ type: types.ADD_TOPIC, text })
-export const removeTopic = id => ({ type: types.REMOVE_TOPIC, id })
-export const addComment = (id, text) => ({ type: types.ADD_COMMENT, id, text})
-export const changeHeader = header => ({ type: types.CHANGE_HEADER, header })
