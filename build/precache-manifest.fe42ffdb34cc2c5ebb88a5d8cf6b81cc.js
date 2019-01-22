5889e7d2367c65c1df0df501e1a559e485535620
aleksandramt <lawrhill.ml@gmail.com>
2019-01-22 10:48:02 +0200

    all

---
+++ build/precache-manifest.fe42ffdb34cc2c5ebb88a5d8cf6b81cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc145e25eaa2428cc3872eb6c2b5c8fb",
+    "url": "/test-app/static/media/send.cc145e25.svg"
+  },
+  {
+    "revision": "bdec716428673b47d290b80460f6a4c1",
+    "url": "/test-app/static/media/back.bdec7164.svg"
+  },
+  {
+    "revision": "3510dee076921078b779",
+    "url": "/test-app/static/js/runtime~main.3510dee0.js"
+  },
+  {
+    "revision": "1f6916123e0786ddeecb",
+    "url": "/test-app/static/js/main.1f691612.chunk.js"
+  },
+  {
+    "revision": "a4f0a912af3230bdeb5d",
+    "url": "/test-app/static/js/1.a4f0a912.chunk.js"
+  },
+  {
+    "revision": "1f6916123e0786ddeecb",
+    "url": "/test-app/static/css/main.4b84c65e.chunk.css"
+  },
+  {
+    "revision": "a9e8cc048175fb4e7b48e894cc365a30",
+    "url": "/test-app/index.html"
+  }
+];
